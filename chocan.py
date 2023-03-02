ab71708c5bc3a4368612fbb22c3d249ad00d15d4
Miles <miles4599@gmail.com>
2023-03-01 16:16:28 -0800

    wrapped chocan.py code in a main() function

--- chocan.py
+++ chocan.py
@@ -1,3 +1,8 @@
 from chocan import output_system
 
-output_system.display("ChocAn System v0.1")
+def main():
+    output_system.display("ChocAn System v0.1")
+
+if __name__ == "__main__":
+    main()
+
