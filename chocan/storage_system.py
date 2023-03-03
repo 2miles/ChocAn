97a393c5b2297c96e6696b8f31acd82b7fcf582c
lee-112 <leegeezy@hotmail.com>
2023-03-03 14:07:30 -0800

    Added functions to storage_system
    Added get_index_of_record and get_record.

--- chocan/storage_system.py
+++ chocan/storage_system.py
@@ -1,6 +1,9 @@
 from enum import Enum
 import json
 
+class InvalidRecordType(Exception):
+    pass
+
 class RecordType(Enum):
     MEMBER   = 1
     PROVIDER = 2
@@ -26,8 +29,24 @@ def get_all_records(record_type : RecordType) -> "list[dict]":
     except FileNotFoundError:
         return []
 
-def create_record(record_type : RecordType, data : dict)-> None:
+def create_record(record_type : RecordType, data : dict) -> None:
     records = get_all_records(record_type)
     records.append(data)
     json.dump(records, open(file_path(record_type), "w"))
     return None
+
+def get_index_of_record(records: list, number: int) -> int:
+    for i in range(len(records)):
+        if records[i]["number"] == number:
+            return i
+    return None
+
+def get_record(record_type: RecordType, number: int) -> "list[dict]":
+    if record_type == RecordType.SERVICE:
+        raise InvalidRecordType("Service records not supported.")
+    records = get_all_records(record_type)
+    record_index = get_index_of_record(records, number)
+    if record_index is not None:
+        return records[record_index]
+    else:
+        return None
