ab8daea4a70bd2cc0dcd86840b15879eda45ce78
lee-112 <leegeezy@hotmail.com>
2023-03-03 19:13:48 -0800

    Update provider_system

--- chocan/provider_system.py
+++ chocan/provider_system.py
@@ -5,26 +5,26 @@ class InvalidProviderNumber(Exception):
 
 class ProviderRecord:
     def __init__(self, data: dict):
-        self.name = data["name"]
+        self.name   = data["name"]
         self.street = data["street"]
-        self.city = data["city"]
-        self.state = data["state"]
-        self.zip = data["zip"]
+        self.city   = data["city"]
+        self.state  = data["state"]
+        self.zip    = data["zip"]
         self.number = data["number"]
 
 def generate_provider_number() -> int:
     members = storage_system.get_all_records(storage_system.RecordType.PROVIDER)
-    number = max(map(lambda r: r["number"], members)) + 1 if members else 1
+    number = max(map(lambda r : r["number"], members)) + 1 if members else 1
     if number > 999999999:
         raise InvalidProviderNumber("Provider number too large: " + str(number))
     return number
 
 def create_provider(
-    name: str,
-    street: str,
-    city: str,
-    state: str,
-    zip: str
+    name   : str,
+    street : str,
+    city   : str,
+    state  : str,
+    zip    : str
 ) -> 'ProviderRecord':
     number = generate_provider_number()
 
@@ -32,12 +32,12 @@ def create_provider(
     data = {
         # Key value pairs for the data field except those with None values.
         key: value for key, value in {
-            "name": name,
-            "street": street,
-            "city": city,
-            "state": state,
-            "zip": zip,
-            "number": number
+            "name"   : name,
+            "street" : street,
+            "city"   : city,
+            "state"  : state,
+            "zip"    : zip,
+            "number" : number
         # Check that the data fields are not None.
         }.items() if value is not None
     }
@@ -46,20 +46,20 @@ def create_provider(
     return provider_record
 
 def update_provider( 
-    name: str, 
-    street: str, 
-    city: str, 
-    state: str, 
-    zip: str,
-    number: int
+    name   : str, 
+    street : str, 
+    city   : str, 
+    state  : str, 
+    zip    : str,
+    number : int
 ) -> 'ProviderRecord':
     data = {
-        "name": name,
-        "street": street,
-        "city": city,
-        "state": state,
-        "zip": zip,
-        "number": number
+        "name"   : name,
+        "street" : street,
+        "city"   : city,
+        "state"  : state,
+        "zip"    : zip,
+        "number" : number
     }
     provider_record = ProviderRecord(data)
     storage_system.update_record(storage_system.RecordType.PROVIDER, number, data)
@@ -67,7 +67,7 @@ def update_provider(
 
 def get_all_providers() -> list['ProviderRecord']:
     records = storage_system.get_all_records(storage_system.RecordType.PROVIDER)
-    records = list(map(lambda r: ProviderRecord(r), records))
+    records = list(map(lambda r : ProviderRecord(r), records))
     return records
 
 def get_provider(number: int) -> list['ProviderRecord']:
