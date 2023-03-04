b63ad15f1e64d22c85b0689c7eb9345f77cb6179
lee-112 <leegeezy@hotmail.com>
2023-03-03 19:16:41 -0800

    Added tests for create_member and get_all_member

--- tests/storage_system_test.py
+++ tests/storage_system_test.py
@@ -1,4 +1,4 @@
-from chocan import storage_system, provider_system
+from chocan import storage_system, provider_system, member_system
 import pytest
 import os
 
@@ -148,3 +148,42 @@ def test_delete_provider(record_cleaner):
     # Check that provider 2 was deleted from the storage system.
     del_provider = provider_system.get_provider(2)
     del_provider == None
+
+def test_create_member(record_cleaner):
+    member = member_system.create_member(
+        name   = "Member 4",
+        street = "3rd Ave",
+        city   = "Portland",
+        state  = "OR",
+        zip    = "34567"
+    )
+    assert member.name   == "Member 4"
+    assert member.street == "3rd Ave"
+    assert member.city   == "Portland"
+    assert member.state  == "OR"
+    assert member.zip    == "34567"
+    assert member.number == 4
+
+def test_get_all_members(record_cleaner):
+    members = member_system.get_all_members()
+    assert len(members) == 3
+    assert members[0].name   == "Patty Tester"
+    assert members[0].street == "123 Alphabet"
+    assert members[0].city   == "Portlandia"
+    assert members[0].state  == "OR"
+    assert members[0].zip    == "97214"
+    assert members[0].number == 1
+
+    assert members[1].name   == "Steven Software"
+    assert members[1].street == "10th Circle"
+    assert members[1].city   == "Vancouver"
+    assert members[1].state  == "WA"
+    assert members[1].zip    == "98686"
+    assert members[1].number == 2
+
+    assert members[2].name   == "Taylor Todds"
+    assert members[2].street == "1900 Morrison St"
+    assert members[2].city   == "Portlandia"
+    assert members[2].state  == "OR"
+    assert members[2].zip    == "97214"
+    assert members[2].number == 3
