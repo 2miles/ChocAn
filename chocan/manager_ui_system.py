b8b0a90246eb103281c94933556748513f562061
Miles <miles4599@gmail.com>
2023-03-11 12:28:37 -0800

    updated show user list functins

--- chocan/manager_ui_system.py
+++ chocan/manager_ui_system.py
@@ -164,8 +164,7 @@ def run_show_member_list_ui() -> None:
     members = member_system.get_all_members()
     output_system.display("\nMEMBERS:")
     for member in members:
-        if member.deleted == False:
-            output_system.display(f"{member.number} \t {member.name}")
+        output_system.display(f"{member.number} \t {member.name}")
 
 
 # Manage Providers Menu
@@ -259,8 +258,7 @@ def run_show_provider_list_ui() -> None:
     providers = provider_system.get_all_providers()
     output_system.display("\nPROVIDERS:")
     for provider in providers:
-        if provider.deleted == False:
-            output_system.display(f"{provider.number} \t {provider.name}")
+        output_system.display(f"{provider.number} \t {provider.name}")
 
 # Generate Reports Menu
 ###############################################################################
