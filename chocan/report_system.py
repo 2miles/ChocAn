007365e082b443ffddb463355d24edf0f035a51b
Robert Peterson <rgp3@pdx.edu>
2023-03-13 12:38:04 -0700

    report system interface TODOs

---
+++ chocan/report_system.py
@@ -0,0 +1,15 @@
+#TODO
+def generate_provider_report() -> None:
+    pass
+
+#TODO
+def generate_member_service_report() -> None:
+    pass
+
+#TODO
+def generate_mgmt_report() -> None:
+    pass
+
+#TODO
+def generate_eft_report() -> None:
+    pass
