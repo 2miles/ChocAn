f3f014d1eb046f1a3ab00d1c5efa60d38ed816a4
Robert Peterson <rgp3@pdx.edu>
2023-03-14 19:49:03 -0700

    remove capitalize

--- chocan/input_system.py
+++ chocan/input_system.py
@@ -1,13 +1,6 @@
 def get_input(max : int) -> str:
-    """
-    Strips the input's leading and trailing whitespace and returns the
-    the first 'max' chars of the stripped input, with the first letter
-    capitalized.
-    """
     if max < 0:
         raise ValueError('max must be a non-negative integer')
     user_input = input()
-    nice_input = user_input.strip().capitalize()
+    nice_input = user_input.strip()
     return nice_input[0:max]
-
-
